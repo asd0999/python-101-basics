15b61ce0caedb8b9a600ed8d861a4fcd478cc990
asd0999 <34610152+asd0999@users.noreply.github.com>
2018-04-25 18:08:27 +0530

    Add files via upload
    more about string variables and duplicating them using ' * ' symbol

---
+++ 2_how_many_snakes.py
@@ -0,0 +1,15 @@
+how_many_snakes = 3
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+"""
+
+
+print(snake_string * how_many_snakes)
