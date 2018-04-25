dcd1ea4d7003abac4ef802598b72a220489fe635
asd0999 <34610152+asd0999@users.noreply.github.com>
2018-04-25 18:05:11 +0530

    Add files via upload
    simple function for printing a welcome message

---
+++ 1_my_first_script.py
@@ -0,0 +1,6 @@
+#Prints out a personalised welcome message
+def welcome_message(name):
+    return "Welcome to this Python script, " + name + "!"
+
+#and print the output
+print(welcome_message("Tushar"))
