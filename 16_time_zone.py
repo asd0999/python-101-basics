32970103c3d82ac18ab98a9d7461849441e29441
Tushar <34610152+asd0999@users.noreply.github.com>
2018-04-28 16:46:05 +0530

    Add files via upload

---
+++ 16_time_zone.py
@@ -0,0 +1,12 @@
+#pip3 install pytz (in terminal)
+from datetime import datetime
+
+import pytz
+
+utc = pytz.utc # utc is Coordinated Universal Time
+ist = pytz.timezone('Asia/Kolkata') #IST is Indian Standard Time
+
+now = datetime.now(tz=utc) # this is the current time in UTC
+ist_now = now.astimezone(ist) # this is the current time in IST.
+
+print(ist_now)
